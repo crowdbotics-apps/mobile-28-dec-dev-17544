64cd4e7be7ed0576b696aa5836dc8f7f6d24b2ec
Crowdbotics <team@crowdbotics.com>
2020-12-28 11:10:42 +0000

    Update store

--- src/store/services.js
+++ src/store/services.js
@@ -1,4 +1,17 @@
 import axios from "axios"
+import {
+  NEW_CONNECTOR_HJGHJGHJ_JHGJG_USERNAME,
+  NEW_CONNECTOR_HJGHJGHJ_JHGJG_PASSWORD
+} from "react-native-dotenv"
+const newConnectorhjghjghjjhgjg = axios.create({
+  baseURL:
+    "https://crowdbotics-slack-dev.herokuapp.com/dashboard/app/17544/storyboard/16425/",
+  auth: {
+    username: NEW_CONNECTOR_HJGHJGHJ_JHGJG_USERNAME,
+    password: NEW_CONNECTOR_HJGHJGHJ_JHGJG_PASSWORD
+  },
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 const mobiledecAPI = axios.create({
   baseURL: "https://mobile-28-dec-dev-17544-prod.herokuapp.com/",
   headers: { Accept: "application/json", "Content-Type": "application/json" }
